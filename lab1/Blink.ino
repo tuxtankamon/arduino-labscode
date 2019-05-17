87b24be57dfbb93f4b92bb3c91d6c322db545108
tuxtankamon <caoh36@yahoo.com>
2019-05-17 11:19:58 -0600

    Update Blink.ino

--- lab1/Blink.ino
+++ lab1/Blink.ino
@@ -1,29 +1,13 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+//Practica para apagar y encender un LED
+int ledPin = 23;//Esta es la variable que guardara el numero del pin
 
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://www.arduino.cc
-
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
- */
-
-int led =12;
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(led, OUTPUT);
+  pinMode(ledPin, OUTPUT);//Define como salida el numero del pin
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(led, LOW);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
+  digitalWrite(ledPin, HIGH);  //Enciende el led en el nivel High (5V)
+  delay(1000);                       // Espera un segundo
+  digitalWrite(ledPin, LOW);    // Enciende el led en el nivel Low (0V)
+  delay(1000);                       // Espera un segundo
 }
